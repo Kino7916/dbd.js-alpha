150d92fa685bf93495968029f541605403755c47
Kino7916 <73807155+Kino7916@users.noreply.github.com>
2021-06-22 07:17:35 +0700

    ResolveMessage Edit#1
    Changed some to be compatible with djs 13 & removed coding that will be handled by `try` and `catch`

--- Handlers/resolveMessage.js
+++ Handlers/resolveMessage.js
@@ -9,13 +9,15 @@ async function resolveMessage(message, client, channelID, content, embed, realCo
   const channel = await client.channels.fetch(channelID, false);
 
   if (!channel) return new Error("Unavailable channel " + String(channelID) + " or not found as it's not available for Client.");
-  const p = channel.permissionsFor(client.user.id).toArray();
-  if (!p.includes("SEND_MESSAGES") && !p.includes("VIEW_CHANNEL")) return new Error("Missing Permissions to Send Messages for Channel " + String(channelID));
-
-  if (!embed.description) embed = null;
-  const msg = await channel.send(content, {embed});
+  
+  if (!embed.description) embed = {};
+  try {
+  const msg = await channel.send(content, {embeds: [embed]});
 
   return msg;
+  } catch (err) {
+    return err
+  }
 }
 
 module.exports = resolveMessage;
